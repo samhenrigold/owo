08e392703db21bf373c186537486bdedd6190412
samhenrigold <49251320+samhenrigold@users.noreply.github.com>
2021-07-04 22:10:54 -0400

    Update owo.js

--- owo.js
+++ owo.js
@@ -1,54 +1,31 @@
 const randomItem = function (arr) {
-	if (!Array.isArray(arr)) {
-		throw new TypeError('Expected an array');
-	}
-
+	if (!Array.isArray(arr)) { throw new TypeError('Expected an array'); }
 	return arr[Math.floor(Math.random() * arr.length)];
 };
 
 
 
 const replaceString = (input, needle, replacement, options = {}) => {
-	if (typeof input !== 'string') {
-		throw new TypeError(`Expected input to be a string, got ${typeof input}`);
-	}
-
+	if (typeof input !== 'string') { throw new TypeError(`Expected input to be a string, got ${typeof input}`); }
 	if (!(typeof needle === 'string' && needle.length > 0) ||
 		!(typeof replacement === 'string' || typeof replacement === 'function')) {
 		return input;
 	}
-
 	let ret = '';
 	let matchCount = 0;
 	let prevIndex = options.fromIndex > 0 ? options.fromIndex : 0;
-
-	if (prevIndex > input.length) {
-		return input;
-	}
-
+	if (prevIndex > input.length) { return input; }
 	while (true) { // eslint-disable-line no-constant-condition
 		const index = input.indexOf(needle, prevIndex);
-
-		if (index === -1) {
-			break;
-		}
-
+		if (index === -1) { break; }
 		matchCount++;
-
 		const replaceStr = typeof replacement === 'string' ? replacement : replacement(needle, matchCount, input, index);
-
 		// Get the initial part of the string on the first iteration
 		const beginSlice = matchCount === 1 ? 0 : prevIndex;
-
 		ret += input.slice(beginSlice, index) + replaceStr;
-
 		prevIndex = index + needle.length;
 	}
-
-	if (matchCount === 0) {
-		return input;
-	}
-
+	if (matchCount === 0) { return input; }
 	return ret + input.slice(prevIndex);
 };
 
@@ -128,9 +105,7 @@ const substitutions = {
 const addAffixes = (str) => randomItem(prefixes) + str + randomItem(suffixes)
 const substitute = (str) => {
   const replacements = Object.keys(substitutions)
-  replacements.forEach((x) => {
-    str = replaceString(str, x, substitutions[x])
-  })
+  replacements.forEach((x) => {  str = replaceString(str, x, substitutions[x])  })
   return str
 }
 const owo = (str) => addAffixes(substitute(str))
